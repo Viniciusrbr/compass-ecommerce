ba0c9c09aee4902a76defc8bc9edba6b08e06e59
Viviane Mori <vivianemori@Vivianes-MacBook-Pro-2.local>
2023-08-23 00:52:07 -0300

    feat: start to add top banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
+import { Fragment } from 'react'
 import HomePage from './pages/HomePage'
 
 function App() {
-
   return (
     <>
       <HomePage />
--- src/components/MainBanner/index.jsx
+++ src/components/MainBanner/index.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import styled from "styled-components";
+import Banner from "../../assets/images/Banner.png"
+import LArrowIcon from "../../assets/icons/arrow-previous.png"
 
-function MainBanner() {
+const BannerContainer = styled.section`
+  background-image: url(${Banner});
+`
+
+const BannerTitle = styled.h1`
+  color: #FFF;
+  font-family: Roboto Mono;
+  font-size: 3rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 4.375rem;
+  text-align: center;
+`;
+
+const BannerCaption = styled.p`
+  color: #F8F8F8;
+  font-family: Roboto Mono;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.5625rem;
+  text-align: center;
+`
+
+const LeftArrowIcon = styled.img`
+
+`
+
+const TopBanner = () => {
   return (
-    <div>MainBanner</div>
-  )
-}
+    <BannerContainer src={Banner} className="top-banner-image" alt="Macbook partially open with an abstract screensaver">
+      <h1>Macbook PRO M2</h1>
+      <p>Velocidade e performance</p>
+      <LeftArrowIcon src={LArrowIcon} alt="Arrow pointing left" />
+    </BannerContainer>
+  );
+};
 
-export default MainBanner
+export default TopBanner;
