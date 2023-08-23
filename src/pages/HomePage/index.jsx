bc932df5fd8a9f588eec76eea245ced34c82bea5
carolina lima <carol.gha@gmail.com>
2023-08-23 17:18:19 -0300

    First session of feature

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/common/Header";
 import MainBanner from '../../components/MainBanner/index';
+import SectionWithDivs from '../../components/FeatureContainer/index';
 
 function HomePage() {
   return <>
     <Header></Header>
     <MainBanner />
+    <SectionWithDivs />
   </>;
 }
 
