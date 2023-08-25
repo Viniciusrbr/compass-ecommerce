a4eb8622a740c8c529f9b9937c4e90b2976d51a2
Viviane Mori <vivianemori@Vivianes-MacBook-Pro-2.local>
2023-08-25 02:29:38 -0300

    feat: get product for offer container from api

--- src/components/OfferContainer/OfferItem/index.jsx
+++ src/components/OfferContainer/OfferItem/index.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import api from "../../../services/api";
 import styled from "styled-components";
 import ProductImage from "../../../assets/images/watch.jpeg";
 import Tag from "../../Tag/index";
 import Countdown from "../../Countdown/index";
-import StarIcon from "../../../assets/icons/star.svg";
 import CartIcon from "../../../assets/icons/cart.svg";
 
 const colors = {
@@ -18,25 +18,7 @@ const colors = {
   white: "#FFFFFF",
 };
 
-const Heart = (
-  <svg
-    width="18"
-    height="16"
-    viewBox="0 0 18 16"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g id="icon">
-      <path
-        id="Vector"
-        d="M13.1484 0.0975342C11.6511 0.0975342 10.3385 0.765186 9.35258 2.02835C9.2211 2.19682 9.10389 2.36536 9 2.52859C8.89611 2.36532 8.7789 2.19682 8.64742 2.02835C7.66146 0.765186 6.34887 0.0975342 4.85156 0.0975342C2.02303 0.0975342 0 2.46591 0 5.30783C0 8.55743 2.66333 11.62 8.63568 15.2381C8.74765 15.3059 8.87382 15.3398 9 15.3398C9.12618 15.3398 9.25235 15.3059 9.36432 15.2381C15.3367 11.6201 18 8.55746 18 5.30787C18 2.46742 15.9786 0.0975342 13.1484 0.0975342ZM14.736 9.35414C13.4926 10.7415 11.6137 12.2023 9 13.8128C6.38631 12.2023 4.50738 10.7415 3.26401 9.35417C2.01393 7.95931 1.40625 6.63575 1.40625 5.30787C1.40625 3.26163 2.78909 1.50378 4.85156 1.50378C5.90189 1.50378 6.79746 1.96046 7.51342 2.86117C8.0859 3.58148 8.32711 4.32528 8.3288 4.33059C8.42038 4.62432 8.69235 4.8244 9.00004 4.8244C9.30772 4.8244 9.57969 4.62436 9.67127 4.33059C9.67349 4.32349 9.90745 3.60289 10.4611 2.89358C11.181 1.97136 12.0851 1.50375 13.1484 1.50375C15.2131 1.50375 16.5938 3.26328 16.5938 5.30783C16.5938 6.63572 15.9861 7.95928 14.736 9.35414Z"
-        fill={colors.mediumGray}
-      />
-    </g>
-  </svg>
-);
-
-const HeartIcon = ({myColor}) => {
+const HeartIcon = ({myColor}) => (
   <svg
     width="18"
     height="16"
@@ -51,8 +33,8 @@ const HeartIcon = ({myColor}) => {
         fill={myColor}
       />
     </g>
-  </svg>;
-};
+  </svg>
+);
 
 const OfferItemContainer = styled.div`
   align-items: flex-start;
@@ -106,8 +88,13 @@ const OfferItemTitle = styled.p`
   font-size: 18px;
   font-style: normal;
   font-weight: 500;
+  height: 25px;
   line-height: 25px;
   margin: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 360px;
 `;
 
 const OfferItemDescription = styled.p`
@@ -120,6 +107,11 @@ const OfferItemDescription = styled.p`
   font-weight: 400;
   line-height: 25px;
   margin: 0;
+  height: 25px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 360px;
 `;
 
 const OfferItemRating = styled.div`
@@ -153,21 +145,27 @@ const OfferItemRatingStars = styled.div`
   width: 73px;
 `;
 
-const OfferItemStarIcon = styled.div`
-  align-items: center;
-  align-self: stretch;
-  background-image: url(${StarIcon});
-  background-position: center center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  color: #d6d6d6;
-  display: flex;
-  height: 13px;
-  padding: 0.299px 0px 0.3px 0px;
-  justify-content: center;
-  margin: 0;
-  width: 13px;
-`;
+const StarIcon = ({myFill}) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="14" viewBox="0 0 13 14" 
+  fill="none">
+  <path d="M12.9814 5.50829C12.9366 5.3704 12.8174 5.2699 12.674 5.24908L8.6439 4.66345L6.84156 1.01161C6.77742 0.881607 6.64501 0.799316 6.50005 0.799316C6.35507 0.799316 6.22269 0.881607 6.15852 1.01161L4.3561 4.66345L0.326118 5.24908C0.182687 5.2699 0.0634781 5.3704 0.0186892 5.50827C-0.0261251 5.64616 0.0112498 5.79752 0.115072 5.8987L3.03112 8.74127L2.34284 12.7551C2.31831 12.898 2.37706 13.0423 2.49434 13.1276C2.56069 13.1758 2.63927 13.2003 2.71824 13.2003C2.77887 13.2003 2.8397 13.1859 2.89541 13.1566L6.50003 11.2615L10.1045 13.1565C10.2328 13.224 10.3883 13.2128 10.5056 13.1276C10.6229 13.0423 10.6816 12.8979 10.6571 12.755L9.96863 8.74127L12.885 5.89867C12.9888 5.79752 13.0262 5.64616 12.9814 5.50829Z" 
+  fill={myFill}/>
+</svg>
+);
+
+const StarIconHalfFilled = () => {
+  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="14" viewBox="0 0 13 14" 
+  fill="none">
+    <defs>
+    <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
+      <stop offset="50%" style="stop-color:rgb(255,198,42);stop-opacity:1" />
+      <stop offset="50%" style="stop-color:rgb(255,255,255);stop-opacity:1" />
+    </linearGradient>
+  </defs>
+  <path d="M12.9814 5.50829C12.9366 5.3704 12.8174 5.2699 12.674 5.24908L8.6439 4.66345L6.84156 1.01161C6.77742 0.881607 6.64501 0.799316 6.50005 0.799316C6.35507 0.799316 6.22269 0.881607 6.15852 1.01161L4.3561 4.66345L0.326118 5.24908C0.182687 5.2699 0.0634781 5.3704 0.0186892 5.50827C-0.0261251 5.64616 0.0112498 5.79752 0.115072 5.8987L3.03112 8.74127L2.34284 12.7551C2.31831 12.898 2.37706 13.0423 2.49434 13.1276C2.56069 13.1758 2.63927 13.2003 2.71824 13.2003C2.77887 13.2003 2.8397 13.1859 2.89541 13.1566L6.50003 11.2615L10.1045 13.1565C10.2328 13.224 10.3883 13.2128 10.5056 13.1276C10.6229 13.0423 10.6816 12.8979 10.6571 12.755L9.96863 8.74127L12.885 5.89867C12.9888 5.79752 13.0262 5.64616 12.9814 5.50829Z" 
+  fill="url(#grad1)"/>
+</svg>
+};
 
 const OfferItemPrices = styled.div`
   display: flex;
@@ -281,26 +279,43 @@ const OfferItem = () => {
   const handleFavorite = () => {
     setFavorited(!isFavorited);
   };
+  if (isFavorited) {
+    HeartIcon.myColor = "#FFFFFF";
+  }
+
+  const [product, setProduct] = useState();
+    useEffect(() => {
+      api
+      .get("/products/2")
+      .then((response) => setProduct(response.data))
+      .catch(error => console.log(error));
+    });
+
+    const transCurrency = function(locale, currency, amount) {
+      return new Intl.NumberFormat(locale, {style: 'currency', currency: 'pt-BR'}).format(amount);
+    };
+
   return (
     <OfferItemContainer>
       <OfferItemDetails>
         <OfferItemTextInfo>
-          <OfferItemTitle>Novo relógio inteligente da série 8</OfferItemTitle>
-          <OfferItemDescription>
-            Black Sport Band - Regular.
+          <OfferItemTitle>{product?.title}</OfferItemTitle>
+          <OfferItemDescription line={1}>
+            {product?.description}
           </OfferItemDescription>
           <OfferItemRating>
-            <OfferItemRatingText>(4.5)</OfferItemRatingText>
+            <OfferItemRatingText>(5.0)</OfferItemRatingText>
             <OfferItemRatingStars>
-              <OfferItemStarIcon />
-              <OfferItemStarIcon />
-              <OfferItemStarIcon />
-              <OfferItemStarIcon />
-              <OfferItemStarIcon />
+              <StarIcon myFill="#FFC62A"></StarIcon>
+              <StarIcon myFill="#FFC62A"></StarIcon>
+              <StarIcon myFill="#FFC62A"></StarIcon>
+              <StarIcon myFill="#FFC62A"></StarIcon>
+              <StarIcon myFill="#FFC62A"></StarIcon>
             </OfferItemRatingStars>
           </OfferItemRating>
           <OfferItemPrices>
-            <OfferItemFullPrice>de R$ 900,00</OfferItemFullPrice>
+            <OfferItemFullPrice>de R$ {product?.price}</OfferItemFullPrice>
+            <StarIconHalfFilled/>
             <OfferItemDiscountedPrice>por R$ 780,00</OfferItemDiscountedPrice>
           </OfferItemPrices>
         </OfferItemTextInfo>
@@ -310,7 +325,7 @@ const OfferItem = () => {
             onClick={handleFavorite}
             style={{ background: isFavorited ? "#62D0B6" : "#FFFFFF" }}
           >
-            <HeartIcon style={{ background: isFavorited ? "#FFFFFF" : "#A5A5A5" }}></HeartIcon>
+            <HeartIcon myColor="#A5A5A5"></HeartIcon>
           </ButtonFavorite>
           <ButtonAddToCart>
             <p>Carrinho</p>
